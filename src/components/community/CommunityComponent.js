ad0acea9798d8e08cd40e552a2d6ad6f85a2c3c2
facobackup <gustavomicaelbarbosa@gmail.com>
2021-01-07 17:26:15 -0300

    Community dedicated page

--- src/components/community/CommunityComponent.js
+++ src/components/community/CommunityComponent.js
@@ -1,15 +1,20 @@
 import React from 'react'
-
 import "./CommunityComponentStyle.css"
 import axios from 'axios'; 
 import { Redirect } from "react-router-dom";
+import TopicComponent from '../topics/TopicComponent'
+import { PrimaryButton, DefaultButton } from 'office-ui-fabric-react';
+import { Persona, PersonaSize } from 'office-ui-fabric-react/lib/Persona';
 
 class CommunityComponent extends React.Component{
     constructor(params){
         super()
         this.state={
             community: {},
-            communityID: params.communityID
+            token: params.token,
+            communityID: params.communityID,
+            members: false,
+            topic: true
         }
     }
 
@@ -30,13 +35,72 @@ class CommunityComponent extends React.Component{
     }
 
     render(){
-        if(this.state.community !== {})
-            return(
-                <div>
-                    Opa
-                </div>
-            )
-        else{
+        if(this.state.community !== {}){
+            switch(true){
+                case this.state.members:{
+                    return(
+                        <div className="community_component_container">
+                            <div className="community_image_container">
+                                <img className='profile_background_image_style' alt="BACKGROUD IMG"src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaNwMYAh1BP0Zhiy6r_gvjMMegcosFo70BUw&usqp=CAU"/>
+                            </div>
+                            <div className="community_action_bar">
+                                <Persona
+                                    {...{
+                                        imageUrl: '',
+                                        text: "COMMUNITY NAME",
+                                        secondaryText:  "YOUR ROLE IN IT"
+                                    }}
+                                    size={PersonaSize.size120}
+                                    imageAlt="Community"
+                                />
+                                <DefaultButton text='Home' href='/' />
+                                <PrimaryButton text='Members And Followers'/>
+                                <DefaultButton text='Topics' onClick={()=>this.setState({
+                                    topic: true,
+                                    members: false
+                                })}/>                                
+                            </div>
+                            <div className="community_content_container">
+                                MEMBERS HERE
+                             {/* <TopicComponent community={true} token={this.state.token} timeline={false} subjectID={this.state.communityID}/> */}
+                            </div>
+                        </div>
+                    )
+                }
+                case this.state.topic:{
+                    return(
+                        <div className="community_component_container">
+                            <div className="community_image_container">
+                                <img className='profile_background_image_style' alt="BACKGROUD IMG"src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaNwMYAh1BP0Zhiy6r_gvjMMegcosFo70BUw&usqp=CAU"/>
+                            </div>
+                            <div className="community_action_bar">
+                                <Persona
+                                    {...{
+                                        imageUrl: '',
+                                        text: "COMMUNITY NAME",
+                                        secondaryText:  "YOUR ROLE IN IT"
+                                    }}
+                                    size={PersonaSize.size120}
+                                    imageAlt="Community"
+                                />
+                                <DefaultButton text='Home' href='/' />
+                                <DefaultButton text='Members And Followers' onClick={()=>this.setState({
+                                    topic: false,
+                                    members: true
+                                })}/>
+                                <PrimaryButton text='Topics'/>
+                            </div>
+                            <div className="community_content_container">
+                                <TopicComponent community={true} token={this.state.token} timeline={false} subjectID={this.state.communityID}/>
+                            </div>
+                        </div>
+                    )
+                }
+            }
+        }
+
+            
+        else{Opa
             alert("Some error ocurred")
             return(
                 <Redirect to='/'/>
