07fec39fca99db2cb8c6f70413d88c8060507065
facobackup <gustavomicaelbarbosa@gmail.com>
2021-01-07 16:20:07 -0300

    Tweaks

--- src/components/topics/TopicComponent.js
+++ src/components/topics/TopicComponent.js
@@ -93,30 +93,34 @@ class TopicComponent extends React.Component{
         return(
             <div>
                 <div className="topic_container">
-                    <div className="topic_creator_persona_container">
-                        <Persona
-                            {...{
-                                imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaNwMYAh1BP0Zhiy6r_gvjMMegcosFo70BUw&usqp=CAU",
-                                text: "CREATOR NAME",
-                                secondaryText: "CREATOR EMAIL"
-                            }}
-                            size={PersonaSize.size48}
-                            imageAlt="Profile Picture"
-                        />
-                    </div>
-                    <div className="topic_fields_container">
-                        <p style={{ fontSize: FontSizes.size18, fontWeight:FontWeights.regular}}>TITLE</p>
-                        <p style={{ fontSize: FontSizes.size16, fontWeight:FontWeights.semilight}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    </div>
-                    <div className="topic_image_container">
-                        <img style={{borderRadius:'8px', width:'100%', height: '100%'}}alt="Topic" src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaNwMYAh1BP0Zhiy6r_gvjMMegcosFo70BUw&usqp=CAU"/>
-                    </div>
-                    <div className="topic_buttons_container">
-                        <DefaultButton text="Like"/> 
-                        <DefaultButton text="Dislike"/> 
-                        <DefaultButton text="Comment"/> 
-                        <DefaultButton text="Share" disabled={true}/> 
-                    </div>
+                    {this.state.topics === [] ? <div></div> : this.state.topics.map(topic => (
+                        <div>
+                            <div className="topic_creator_persona_container">
+                                <Persona
+                                    {...{
+                                        imageUrl:(topic.subjectImageURL === null) ?  topic.subjectImageURL.imageURL :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaNwMYAh1BP0Zhiy6r_gvjMMegcosFo70BUw&usqp=CAU",
+                                        text: topic.subjectName,
+                                        secondaryText: (topic.communityID !== null ? topic.communityName : topic.creatorID)
+                                    }}
+                                    size={PersonaSize.size48}
+                                    imageAlt="User"
+                                />
+                            </div>
+                            <div className="topic_fields_container">
+                                <p style={{ fontSize: FontSizes.size18, fontWeight:FontWeights.regular}}>TITLE</p>
+                                <p style={{ fontSize: FontSizes.size16, fontWeight:FontWeights.semilight}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            </div>
+                            <div className="topic_image_container">
+                                <img style={{borderRadius:'8px', width:'100%', height: '100%'}}alt="Topic" src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaNwMYAh1BP0Zhiy6r_gvjMMegcosFo70BUw&usqp=CAU"/>
+                            </div>
+                            <div className="topic_buttons_container">
+                                <DefaultButton text="Like" disabled={true}/> 
+                                <DefaultButton text="Dislike" disabled={true}/> 
+                                <DefaultButton text="Comment" disabled={true}/> 
+                                <DefaultButton text="Share" disabled={true}/> 
+                            </div>
+                        </div>
+                    ))}
                 </div>
                 <div className="timeline_end_container">
                     <p style={{ fontSize: FontSizes.size18, fontWeight:FontWeights.regular}}>You are all caught up for now</p>
