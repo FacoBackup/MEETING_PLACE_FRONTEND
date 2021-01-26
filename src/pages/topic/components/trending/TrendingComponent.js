dc514ade79214528fa4330739a5d3d9f2cb77849
facobackup <gustavomicaelbarbosa@gmail.com>
2021-01-26 18:02:06 -0300

    Trending component

--- src/pages/topic/components/trending/TrendingComponent.js
+++ src/pages/topic/components/trending/TrendingComponent.js
@@ -1,6 +1,34 @@
 import React from 'react'
+import axios from 'axios'
+import Host from '../../../../Host'
+import Cookies from 'universal-cookie'
 
 export default class TrendingComponent extends React.Component{
+    constructor(){
+        super(null)
+        this.state={
+            trending: []
+        }
+    }
+
+    async fetchTrending() {
+        try{
+            await axios({
+                method: 'get',
+                url: Host() + 'api/fetch/trending',
+                headers: {"Authorization": 'Bearer ' + (new Cookies()).get("JWT")}
+            }).then(res => {
+                this.setState({
+                    trending: res.data
+                })
+            }).catch(error => {
+                console.log(error)
+            });
+
+        }catch (e) {
+            console.log(e)
+        }
+    }
     render(){
         return(
             <div className={"right_bottom_half_container"}>
